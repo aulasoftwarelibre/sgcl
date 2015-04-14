7ab15bfe83fa3281fcbe0cfc941ac6949e6a574a
Joaquín Millán <jmibla@gmail.com>
2015-04-14 23:25:30 +0200

    Resuelta la captura dinámica desde EDITAR en PRODUCT y BARCODE. Aún no terminada la creación de BARCODE mediante el asistente.

---
+++ src/AppBundle/Doctrine/ORM/CompanyRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jmb
+ * Date: 13/04/15
+ * Time: 0:09
+ */
+
+namespace AppBundle\Doctrine\ORM;
+
+use AppBundle\Entity;
+use Doctrine\ORM\Query\Expr;
+
+class CompanyRepository extends CustomRepository
+{
+
+}
